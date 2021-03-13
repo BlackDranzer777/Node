52b32e3f4cda687f1781180f61edbf930f9a600d
BlackDranzer777 <35491850+BlackDranzer777@users.noreply.github.com>
2021-03-13 16:36:34 +0530

    Add files via upload

--- Inventory/routes/user.js
+++ Inventory/routes/user.js
@@ -18,9 +18,9 @@ const verifyTokenMiddleWare = require('../middlewares/verifyToken');
 /* === User Routes === */
 
 //Get Request
-router.get('/fetchuser',verifyTokenMiddleWare.verifyToken, userController.getReadUser);
+router.get('/fetchuser', verifyTokenMiddleWare.verifyToken , userController.getReadUser);
 
-router.get('/deleteuser:uid',verifyTokenMiddleWare.verifyToken, userController.getDeleteUser);
+router.get('/deleteuser:uid',verifyTokenMiddleWare.verifyToken , userController.getDeleteUser);
 
 //Delete Request
 router.delete('/logout', userController.logout);
@@ -28,95 +28,92 @@ router.delete('/logout', userController.logout);
 //Post Request
 router.post('/signup', userController.postCreateUser);
 
-router.post('/updateuser',verifyTokenMiddleWare.verifyToken, userController.postUpdateUser);
+router.post('/updateuser', verifyTokenMiddleWare.verifyToken, userController.postUpdateUser);
 
 router.post('/login', userController.postLogin);
 
-router.post('/refreshtoken', userController.postRefreshToken);
-
-//Testing API's  for Development
-router.get('/test/fetchusers', userController.getAllUser);
+// router.post('/refreshtoken', userController.postRefreshToken);
 
 
 
 /* === Menu Routes === */
 
 //POST Request
-router.post('/menu/newitem', menuController.postNewItem);
+router.post('/menu/newitem',verifyTokenMiddleWare.verifyToken, menuController.postNewItem);
 
-router.post('/menu/fetchmenu', menuController.fetchMenu);
+router.post('/menu/fetchmenu',verifyTokenMiddleWare.verifyToken, menuController.fetchMenu);
 
-router.post('/menu/updateitem', menuController.updateNewItem);
+router.post('/menu/updateitem',verifyTokenMiddleWare.verifyToken, menuController.updateNewItem);
 
-router.delete('/menu/deleteitem/:item_id', menuController.deleteItem);
+router.delete('/menu/deleteitem/:item_id',verifyTokenMiddleWare.verifyToken, menuController.deleteItem);
 
 
 /* === Restaurant Routes === */
 
 //POST Request
-router.post('/restaurant/new', restaurantController.postCreateRestaurant);
+router.post('/restaurant/new',verifyTokenMiddleWare.verifyToken, restaurantController.postCreateRestaurant);
 
-router.post('/restaurant/fetchall', restaurantController.postReadRestaurant);
+router.post('/restaurant/fetchall',verifyTokenMiddleWare.verifyToken, restaurantController.postReadRestaurant);
 
-router.post('/restaurant/update', restaurantController.postUpdateRestaurant);
+router.post('/restaurant/update',verifyTokenMiddleWare.verifyToken, restaurantController.postUpdateRestaurant);
 
 //DELETE Request
-router.delete('/restaurant/delete/:restaurant_id', restaurantController.postDeleteRestaurant);
+router.delete('/restaurant/delete/:restaurant_id',verifyTokenMiddleWare.verifyToken, restaurantController.postDeleteRestaurant);
 
 
 
 /* === Outlet Routes === */
 
 //POST Request
-router.post('/outlet/new', outletController.postCreateOutlet);
+router.post('/outlet/new',verifyTokenMiddleWare.verifyToken, outletController.postCreateOutlet);
 
-router.post('/outlet/fetchall', outletController.postReadOutlet);
+router.post('/outlet/fetchall',verifyTokenMiddleWare.verifyToken, outletController.postReadOutlet);
 
-router.post('/outlet/update', outletController.postUpdateOutlet);
+router.post('/outlet/update',verifyTokenMiddleWare.verifyToken, outletController.postUpdateOutlet);
 
 //DELETE Request
-router.delete('/outlet/delete/:outlet_id', outletController.deleteDeleteOutlet);
+router.delete('/outlet/delete/:outlet_id',verifyTokenMiddleWare.verifyToken, outletController.deleteDeleteOutlet);
 
 
 /* === Employee Routes === */
-router.post('/employee/new', employeeController.postNewEmployee); //tested
+router.post('/employee/new',verifyTokenMiddleWare.verifyToken, employeeController.postNewEmployee); //tested
 
-router.post('/employee/fetchall', employeeController.postFetchEmployee); //tested
+router.post('/employee/fetchall',verifyTokenMiddleWare.verifyToken, employeeController.postFetchEmployee); //tested
 
-router.post('/employee/update', employeeController.postUpdateEmployee);
+router.post('/employee/update',verifyTokenMiddleWare.verifyToken, employeeController.postUpdateEmployee);
 
 //DELETE Request
-router.delete('/employee/delete/:employee_id', employeeController.deleteDeleteEmployee);
+router.delete('/employee/delete/:employee_id',verifyTokenMiddleWare.verifyToken, employeeController.deleteDeleteEmployee);
 
 
 /* === Customer Routes === */
-router.post('/customer/new', customerController.postNewCustomer); 
+router.post('/customer/new',verifyTokenMiddleWare.verifyToken, customerController.postNewCustomer); 
 
-router.post('/customer/fetchall/outlet/:outlet_id', customerController.postFetchCustomerByOutlet);
+router.post('/customer/fetchall/outlet/:outlet_id',verifyTokenMiddleWare.verifyToken, customerController.postFetchCustomerByOutlet);
 
-router.post('/customer/fetchall/customer/:customer_id', customerController.postFetchCustomerByCustomerID);
+router.post('/customer/fetchall/customer/:customer_id',verifyTokenMiddleWare.verifyToken, customerController.postFetchCustomerByCustomerID);
 
-router.post('/customer/update', customerController.postUpdateCustomer);
+router.post('/customer/update',verifyTokenMiddleWare.verifyToken, customerController.postUpdateCustomer);
 
 
 //DELETE Request
-router.delete('/customer/delete/:customer_id', customerController.deleteDeleteCustomer);
+router.delete('/customer/delete/:customer_id',verifyTokenMiddleWare.verifyToken, customerController.deleteDeleteCustomer);
 
 
 /* === Order Routes === */
 
 //POST Request
-router.post('/order/new', orderController.postNewOrder); 
+router.post('/order/new',verifyTokenMiddleWare.verifyToken, orderController.postNewOrder); 
 
-router.post('/order/fetchall/outlet/:outlet_id', orderController.postFetchOrderByOutlet );
+router.post('/order/fetchall/outlet/:outlet_id',verifyTokenMiddleWare.verifyToken, orderController.postFetchOrderByOutlet );
 
-router.post('/order/fetchall/customer/:customer_id', orderController.postFetchOrderByCustomerID );
+router.post('/order/fetchall/customer/:customer_id',verifyTokenMiddleWare.verifyToken, orderController.postFetchOrderByCustomerID );
 
-router.post('/order/fetch/:order_id', orderController.postFetchOrderByOrderID);
+router.post('/order/fetch/:order_id',verifyTokenMiddleWare.verifyToken, orderController.postFetchOrderByOrderID);
 
-router.post('/order/update', orderController.postUpdateOrder);
+router.post('/order/update',verifyTokenMiddleWare.verifyToken, orderController.postUpdateOrder);
 
 //DELETE Request
-router.delete('/customer/delete/:order_id', orderController.deleteDeleteOrder);
+router.delete('/customer/delete/:order_id',verifyTokenMiddleWare.verifyToken, orderController.deleteDeleteOrder);
 
 module.exports = router;
